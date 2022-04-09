d233ea0d17020ad9e84d62d8f12decee3760e397
gilpop8663 <wolfye@naver.com>
2022-04-09 22:51:13 +0900

    Add: 초기 세팅 - 2 / twilwind 설치 및 세팅

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return null;
+  return (
+    <div className="bg-sky-900">
+      <h1 className="text-black shadow-sm">it works</h1>
+    </div>
+  );
 };
 
 export default Home;
